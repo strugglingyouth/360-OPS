e22e2133905c0746e92583190e59188ed6570ee7
struggling <630441839@qq.com>
2016-03-28 18:53:58 +0800

    Create 6.sh

---
+++ 6.sh
@@ -0,0 +1,12 @@
+#6.sysinfo_recorder :
+写一个本地服务，定时的搜集系统的cpu使用情况并记录下来。
+
+需要考虑到cup的使用情况包括哪项信息，用什么方式存储数据（记录文件的大小不能一直增长，记录的数据易于展示）。
+
+##answer
+
+答案见：Issues 6.sh
+
+https://github.com/strugglingyouth/360-OPS/issues/4
+
+脚本见：6.sh
