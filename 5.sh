756921e21701cba83b7025748cfa6eff5d93b104
struggling <630441839@qq.com>
2016-03-28 16:59:09 +0800

    Create 5.sh

---
+++ 5.sh
@@ -0,0 +1,37 @@
+sshtunnel.sh 判断是否连接vpn，若没有连接，则会调用 ssh1.exp 连接vpn
+
+##  sshtunnel.sh
+
+>#!/bin/bash
+>
+>while true;do
+>
+>    ps aux | grep  ssh1.exp | grep -v grep
+>
+>    if [ $? -ne 0 ];then
+>        /usr/bin/expect /root/shell/ssh1.exp
+>    fi
+>
+>done
+>
+>
+
+##  ssh1.exp
+>#!/usr/bin/expect 
+>  
+>set port 22 
+>set user feiyu  
+>set host 222.24.51.147  
+>set password feiyu123  
+>set timeout -1  
+>  
+>set timeout -1
+>
+>spawn ssh -4 -p $port $user@$host 
+>expect {
+>    "yes/no" { send "yes\r";exp_continue}
+>    "*password:*" {send "${password}\r";exp_continue}
+>    }
+>expect eof   
+> 
+> 
