65fdd018774f38281841dd33ed55d2aa49344e2a
strugglingyouth <630441839@qq.com>
2016-06-18 12:21:44 +0800

    update 4.sh

--- 4.sh
+++ 4.sh
@@ -25,6 +25,39 @@ cat $Log | while read line;do
     echo $line >> /tmp/log2/${Year}-${Month}-${Day}
 done
 
+
+shell 改进版：
+
+
+#!/bin/bash
+ 
+Usage(){
+    echo "Usage: -bash Logfile"
+}
+         
+if [ 0 -eq 0 ] ;then
+    Usage
+    exit 0
+else
+    Log=
+fi
+ 
+date_log=/tmp/tmp.iOL3hbCEwu
+ 
+cat  |awk -F'[ :]' '{print }'|awk -F'[' '{print }'|uniq > date_log
+ 
+for i in 
+do
+grep   > /tmp/log/--.access
+ 
+done
+ 
+rm -f date_log
+
+
+
+
+
 下面是python多线程版本的：
 
 #!/usr/bin/env python
@@ -53,4 +86,26 @@ if __name__ == '__main__':
 
 
 
+python 奇技淫巧：
 
+
+#!/usr/bin/env python
+# coding=utf-8
+ 
+import threading 
+import re
+from multiprocessing.dummy import Pool as ThreadPool
+ 
+pattern = re.compile(r'.*\[(\d+)\/(\w+)\/(\d+)\:.*')
+ 
+def parse_log(file_line):
+    resu = pattern.match(file_line)
+    day,month,year = resu.groups()
+    
+    with open('/tmp/log/%s-%s-%s' %(year,month,day),'a') as f:
+        f.write(file_line)
+ 
+if __name__ == '__main__':
+    pool = ThreadPool(4)
+    with open('/tmp/access.log') as f:
+           pool.map(parse_log,f)
